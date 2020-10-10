c6cf6a88563b626387540afe133a64ae3f9d0c19
yl1462 <yliang1462@hotmail.com>
2020-10-10 16:30:25 -0500

    meh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip';
+
+// make 2 tooltips here and another inside the App directly
+const firstTooltip = (
+  <Tooltip message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
 
 function App() {
   return (
     <main className='App'>
-      <Split className='left' flexBasis={2}>
+      <Split className='left' flexBasis='2'>
         This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
       </Split>
       <Split className='right'>
---
+++ src/composition/Tooltip.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import React from 'react';
+import './Tooltip.css';
+
+function Tooltip(props) {
+  return (
+    <span className='Tooltip'>
+      <span className='Tooltip-content'>
+        {props.children}
+      </span>
+      <div className='Tooltip-message'>
+        {props.message}
+      </div>
+    </span>
+  )
+}
+
+export default Tooltip;
